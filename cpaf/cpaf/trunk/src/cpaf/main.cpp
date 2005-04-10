bc08cc5f7c891c8d89753ff49c5ea8d8ba78dfff
snakechomp <snakechomp@fe673e2c-04f4-0310-aeff-c8df2449c11c>
2005-04-10 14:14:49 +0000

    Removed last boost dependancy by replacing boost::scoper_ptr with std::auto_ptr. The auto_ptr is never and will never be assigned, so this is ok.

--- cpaf/cpaf/trunk/src/cpaf/main.cpp
+++ cpaf/cpaf/trunk/src/cpaf/main.cpp
@@ -3,7 +3,8 @@ win32 entry function implementation
 */
 
 #include <cpaf/main.h>
-#include <boost/scoped_ptr.hpp>
+//#include <boost/scoped_ptr.hpp>
+#include <memory> // for auto_ptr
 
 // \todo Construct the command line vector and pass it to main()
 
@@ -14,7 +15,8 @@ int cpaf::entry(cpaf::main_ptr main, HINSTANCE hInstance, HINSTANCE hPrevInstanc
     cpaf::App::cmd_line cmd;
 
     // get an application instance
-    boost::scoped_ptr<cpaf::App> app(main(cmd));
+    //boost::scoped_ptr<cpaf::App> app(main(cmd));
+    std::auto_ptr<cpaf::App> app(main(cmd));
 
     app->set_cmd_line(cmd);
     app->init();
@@ -33,7 +35,8 @@ int cpaf::entry(cpaf::main_ptr main, int argc, char *argv[])
     cpaf::App::cmd_line cmd;
 
     // get an application instance
-    boost::scoped_ptr<cpaf::App> app(main(cmd));
+    //boost::scoped_ptr<cpaf::App> app(main(cmd));
+    std::auto_ptr<cpaf::App> app(main(cmd));
 
     app->set_cmd_line(cmd);
     app->init();
