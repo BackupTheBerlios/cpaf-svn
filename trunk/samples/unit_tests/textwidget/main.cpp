8d071e8852b9d4aaec885c61ebb03108f940593c
tomz <tomz@fe673e2c-04f4-0310-aeff-c8df2449c11c>
2005-07-26 14:43:24 +0000

    Added a lot of widgets to the TextWidget unit test

--- trunk/samples/unit_tests/textwidget/main.cpp
+++ trunk/samples/unit_tests/textwidget/main.cpp
@@ -21,11 +21,12 @@
 
 using cpaf::gui::factory::create_widget;
 using namespace cpaf::event;
+using namespace cpaf::gui;
 
 /*
     Our derived application class
 */
-class MyApp : public cpaf::gui::App
+class MyApp : public App
 {
 public:
     bool init();
@@ -36,40 +37,137 @@ public:
 */
 bool MyApp::init()
 {
-    cpaf::gui::Window *wnd = create_widget<cpaf::gui::Window>(
-        cpaf::gui::Window::Initializer()
+   Window *wnd = create_widget<Window>(
+       Window::Initializer()
         .title("Cpaf")
-        .client_size(cpaf::Size(400,400))
+        .client_size(cpaf::Size(450,450))
         );
 
-    cpaf::gui::Button *btn = create_widget<cpaf::gui::Button>(cpaf::gui::Button::Initializer()
-        .parent(wnd)
-        .label("A button")
-        .size(cpaf::Size(200,40))
-        .position(cpaf::Point(50,50))
+    Button::Initializer btn_init;
+    EntryBox::Initializer entry_init;
+    btn_init.parent(wnd);
+    entry_init.parent(wnd);
+
+    Button *get_text = create_widget<Button>(btn_init
+        .label("Get text")
+        .size(cpaf::Size(100,30))
+        .position(cpaf::Point(10,10))
+        .show()
+        );
+    Button *get_selection_range = create_widget<Button>(btn_init
+        .label("Get selection range")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(120,10))
+        .show()
+        );
+    Button *get_selection_bounds = create_widget<Button>(btn_init
+        .label("Get selection bounds")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(280,10))
+        .show()
+        );
+
+    Button *get_insertion_point = create_widget<Button>(btn_init
+        .label("Get insertion point")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(10,45))
+        .show()
+        );
+    Button *get_length = create_widget<Button>(btn_init
+        .label("Get length")
+        .size(cpaf::Size(100,30))
+        .position(cpaf::Point(170,45))
+        .show()
+        );
+    Button *toggle_read_only = create_widget<Button>(btn_init
+        .label("Toggle read only")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(280,45))
+        .show()
+        );
+
+
+    Button *get_text_in_range = create_widget<Button>(btn_init
+        .label("Get text in range")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(10,100))
+        .show()
+        );
+    Button *set_selection_range = create_widget<Button>(btn_init
+        .label("Set selection range")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(10,135))
+        .show()
+        );
+    Button *set_selection_bounds = create_widget<Button>(btn_init
+        .label("Set selection bounds")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(10,170))
+        .show()
+        );
+    Button *set_insertion_point = create_widget<Button>(btn_init
+        .label("Set insertion point")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(170,100))
+        .show()
+        );
+    Button *delete_range = create_widget<Button>(btn_init
+        .label("Delete range")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(170,135))
+        .show()
+        );
+    Button *insert = create_widget<Button>(btn_init
+        .label("Insert")
+        .size(cpaf::Size(75,30))
+        .position(cpaf::Point(170,170))
+        .show()
+        );
+    Button *set_max_length = create_widget<Button>(btn_init
+        .label("Set max length")
+        .size(cpaf::Size(150,30))
+        .position(cpaf::Point(10,205))
+        .show()
+        );
+
+    EntryBox *range_begin = create_widget<EntryBox>(entry_init
+        .text("3")
+        .position(cpaf::Point(330,100))
+        .size(cpaf::Size(50,30))
+        .show()
+        );
+    EntryBox *range_end = create_widget<EntryBox>(entry_init
+        .text("5")
+        .position(cpaf::Point(390,100))
+        .show()
+        );
+
+    EntryBox *insert_text = create_widget<EntryBox>(entry_init
+        .text("Text to insert")
+        .position(cpaf::Point(255, 170))
+        .size(cpaf::Size(185,30))
         .show()
         );
 
     /*
         Create an EntryBox
     */
-    cpaf::gui::EntryBox *entry = create_widget<cpaf::gui::EntryBox>(cpaf::gui::EntryBox::Initializer()
+    EntryBox *entry = create_widget<EntryBox>(EntryBox::Initializer()
         .parent(wnd)
         .text("I'm an entry box!")
-        .position(cpaf::Point(10,150))
+        .position(cpaf::Point(10,260))
         .size(cpaf::Size(200,30))
-        .password_mode(false)
         .show()
         );
 
     /*
         Create a TextBox
     */
-    cpaf::gui::TextBox *text = create_widget<cpaf::gui::TextBox>(cpaf::gui::TextBox::Initializer()
+    TextBox *text = create_widget<TextBox>(TextBox::Initializer()
         .parent(wnd)
         .text("I'm a multline text box!\nHere's the second line\n\nLorem ipsum dolor sit amet, sed consectetuer adipiscing elit.")
-        .position(cpaf::Point(10,200))
-        .size(cpaf::Size(300,100))
+        .position(cpaf::Point(10,300))
+        .size(cpaf::Size(300,130))
         .show()
         );
 
