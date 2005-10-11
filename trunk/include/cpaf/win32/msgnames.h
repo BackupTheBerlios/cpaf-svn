51894d1233c0d47682f24534ba4be3e4d902ded7
snakechomp <snakechomp@fe673e2c-04f4-0310-aeff-c8df2449c11c>
2005-10-11 20:54:00 +0000

    Removing my copyright information from this file because I don't really remember where I got this array from and whether or not it was in the public domain, but its really not mine.

--- trunk/include/cpaf/win32/msgnames.h
+++ trunk/include/cpaf/win32/msgnames.h
@@ -18,10 +18,6 @@
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-    Copyright (C) 2005 Robin McNeill
-    http://www.snakesoft.net
-    snakechomp (at) snakesoft [dot] net
 */
 
 namespace cpaf {
